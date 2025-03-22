bd5fdab6632b54a629e3aa1aa11d10345c1557e6
dashBuresz <csaba.szijarto03@gmail.com>
2025-03-22 18:10:24 +0100

    added utility, started making bfs, it is needed for the graph class,

--- src/main/java/algorithmVisualizer/algorithms/BFS.java
+++ src/main/java/algorithmVisualizer/algorithms/BFS.java
@@ -1,17 +1,42 @@
 package algorithmVisualizer.algorithms;
 
+import algorithmVisualizer.utils.Graph;
+import algorithmVisualizer.utils.Vertex;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
 public class BFS {
     //TODO Implement Breadth First Search Algorithm
     /*
     * we will need a FIFO queue, and the starting vertex, the end result should be a spanning tree of our original graph
     * how bfs works:
     *   we select a vertex at the start
-    *   we but its neightbours in the queue
+    *   we but its neighbours in the queue
     *   we mark the vertex as complete, and we pop it from the queue
     *   after this in a cycle
-    *       we add the neighbours of our currently active vertex to the queue wich are not yet in the queue
-    *       we mark the veretx as complete and we move on, we do this until the queue is empty
+    *       we add the neighbours of our currently active vertex to the queue which are not yet in the queue
+    *       we mark the vertex as complete and we move on, we do this until the queue is empty
     *   done :)
     * */
+    private Graph bfsSpanningTree;
+    private Graph graph;
+    private ArrayDeque<Vertex> bfsQueue;
+    private ArrayList<Vertex> finished;
+    private Vertex active;
+    public BFS(Graph graph, Vertex start)
+    {
+        active = start;
+        bfsSpanningTree = new Graph(graph.getNumberOfVertices(), graph.getNumberOfVertices()-1, graph.directed(), graph.weighted());
+        this.graph = graph;
+    }
+    public Graph runFullBFS()
+    {
 
+        return bfsSpanningTree;
+    }
+    public void stepBFS()
+    {
+
+    }
 }
--- src/main/java/algorithmVisualizer/utils/Graph.java
+++ src/main/java/algorithmVisualizer/utils/Graph.java
@@ -1,57 +1,119 @@
 package algorithmVisualizer.utils;
 
-import java.beans.PropertyEditorManager;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Graph {
     private int n, m;
     private final boolean directed;
     private final boolean weighted;
-    private ArrayList<ArrayList<Vertex>> adjasencyList;
-    private ArrayList<Vertex> verteces;
+    private ArrayList<ArrayList<Vertex>> adjacencyList;
+    private ArrayList<Vertex> vertices;
     private ArrayList<Edge> edges;
 
-    Graph(int n, int m, boolean directed, boolean weighted)
+    public Graph(int n, int m, boolean directed, boolean weighted)
     {
         this.n = n;
         this.m = m;
         this.directed = directed;
         this.weighted = weighted;
     }
-    public void generateGraph(boolean directed, boolean weighted, boolean isTree)
+    public void generateGraph(boolean isTree, boolean negativeWeights)
     {
-        //adding n new verteces to the graph
+        Random random = new Random();
+        //adding n new vertices to the graph
         for (int i = 0; i < n; i++)
         {
-            verteces.add(new Vertex(i + 1));
+            vertices.add(new Vertex(i + 1));
         }
+        int random1, random2;
+        Graph builderGraph = new Graph(n, m, directed, weighted);
+        builderGraph.vertices = this.vertices;
+        //if the graph is a tree
+        // since trees have n-1 edges, and no cycles we can create a tree first
+        // and then add more edges if the graph is not a tree,
+        // oh but wait that's not good. What if we want a non-connected graph? Or do we even care? No we don't.
+        if (isTree)
+        {
+            while (edges.size() <= n - 1) {
+                random1 =  random.nextInt(n);
+                random2 =  random.nextInt(n);
+                if (random1 != random2)
+                {
+
+                }
+            }
+        }
+        //if the graph isn't a tree
+        if (!isTree)
+        {
+            while(edges.size() <= m)
+            {
+                random1 =  random.nextInt(n);
+                random2 =  random.nextInt(n);
+                if (random1 != random2)
+                {
+
+                }
+            }
+        }
+
+
+        //if the edges are weighted we assign them values randomly (depending on if there is negative weight or not)
+
     }
 //getter setter
     public int getNumberOfEdges() {return m;}
-    public int getNumberOfNodes() {return n;}
+    public int getNumberOfVertices() {return n;}
     public boolean directed() { return directed;}
     public boolean weighted() { return weighted;}
-    public ArrayList<Vertex> getVerteces() {return verteces;}
+    public ArrayList<Vertex> getVertices() {return vertices;}
     public ArrayList<Edge> getEdges() {return edges;}
+//Utilities
     public void addEdge(Edge newEdge)
     {
         edges.add(newEdge);
+        m = m + 1;
     }
     public void addEdge(Vertex start, Vertex end, int weight)
     {
         if (start != null && end != null) edges.add(new Edge(start, end, weight));
+        m = m + 1;
     }
-    public void addVertex(Vertex newVertex)
+    public void addVertex()
     {
-        verteces.add(newVertex);
+        Vertex newVertex = new Vertex(vertices.size());
+        vertices.add(newVertex);
+        n = n + 1;
     }
     public void removeEdge(Edge edge)
     {
         edges.remove(edge);
+        m = m - 1;
     }
     public void removeVertex(Vertex vertex)
     {
         //we also need to remove all the edges that connect to this vertex
-
+        ArrayList<Edge> edgesToRemove = new ArrayList<>();
+        for (int i = 0; i < m; i++)
+        {
+            if (edges.get(i).getStart().equals(vertex) || edges.get(i).getEnd().equals(vertex)) edgesToRemove.add(edges.get(i));
+        }
+        edges.removeAll(edgesToRemove);
+        vertices.remove(vertex);
     }
+    public ArrayList<Vertex> findAdjacentVertices(Vertex vertex)
+    {
+        ArrayList<Vertex> adjacentVertices = new ArrayList<>();
+        for (int i = 0; i < adjacencyList.size(); i++)
+        {
+            if (vertex.id() == adjacencyList.get(i).get(0).id())
+            {
+                adjacentVertices.addAll(adjacencyList.get(i).subList(1, adjacencyList.get(i).size()));
+                break;
+            }
+        }
+        return adjacentVertices;
+    }
+
 }
